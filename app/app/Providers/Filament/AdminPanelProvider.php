ab4516215d4b9310f31d236e793c0a8e68858c8d
root <magbusjap@gmail.com>
2026-03-28 18:22:57 +0000

    fix: bugs and login issues

--- app/app/Providers/Filament/AdminPanelProvider.php
+++ app/app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('magbusjap')
             ->login()
             ->profile(isSimple: false)
             ->colors([
